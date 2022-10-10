1e0a642b7dfa3fa8b65dc63e9df145f75daa7349
marefpceo <marefpceo@gmail.com>
2022-10-10 14:35:46 -0700

    Completed the game and updated README
    Completed the rock, paper, scissor game in main.js.
    
    Updated README.md to accurately reflect the game with instructions on how
    to play.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # rock-papaer-scissors
 Rock Paper Scissors game 
 
-Simple implementation of the classic "rock, paper, scissors" game using Javascript.
+Simple implementation of the classic "rock, paper, scissors" game using the Javascript console.
+
+The game prompts the user to enter "Rock", "Paper", or "Scissor" and the computer will generate a random selection. 
+The two selection will be compared and the winner will be displayed in the console. The game will score the best 
+out of 5 and declare the winner after completing the 5th round. 
--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 //rock-paper-scissors game will be played against the computer
 //The game will consist of a single round of game play. 
 
+
+let playerScore = 0;
+let computerScore = 0;
+
+
 //Function that randomly makes computer selection
 function getComputerChoice() {
     let x = Math.floor(Math.random() * 3);
@@ -19,63 +24,95 @@ function getComputerChoice() {
 }
 
 
-//Prompt the player to make a selection
-const playerSelection = prompt("Choose Rock, Paper, or Scissor");
-
-//The computer's selection will be randomly generated 
-const computerSelection = getComputerChoice();
-
-//A winner will be declared based on the two selections
-//Rock beats Scissors
-//Paper beats Rock
-//Scissor beat Paper
-
-function playRound(playerSelection, computerSelection){
-    let player = playerSelection.toLowerCase();
-
-
-    if ((playerSelection !== "rock") && (playerSelection !== "paper") && (playerSelection !== "scissor")) {
+//Helper function to verify player input. Improper inputs return a value of false. 
+function inputVerify(playerInput){
+    if ((playerInput !== "rock") && (playerInput !== "paper") && (playerInput !== "scissor")) {
         alert("Please enter a valid selection!");
-        location.reload();
+        return false;
     }
+}
 
-    switch (player) {
+
+
+//Function to play a single round of the rock, paper, scissor game
+function playRound(playerSelection, computerSelection){
+
+    switch (playerSelection) {
         case "rock":
-            if (player === computerSelection) {
+            if (playerSelection === computerSelection) {
                 return "The game was tied. You both selected Rock";
             } 
 
             if (computerSelection === "scissor") {
+                playerScore++;
                 return "You win! Rock beats Scissor!";
             } else {
+                computerScore++;
                 return "You lose. Paper beats Rock.";
             }
             break;
     
         case "paper":
-            if (player === computerSelection) {
+            if (playerSelection === computerSelection) {
                 return "The game was tied. You both selected Paper";
             } 
 
             if (computerSelection === "rock") {
+                playerScore++;
                 return "You win! Paper beats Rock!";
             } else {
+                computerScore++;
                 return "You lose. Scissor beats Paper.";
             }
             break;
 
         case "scissor":
-            if (player === computerSelection) {
+            if (playerSelection === computerSelection) {
                 return "The game was tied. You both selected Scissor";
             } 
 
             if (computerSelection === "paper") {
+                playerScore++;
                 return "You win! Scissor beats Paper!";
             } else {
+                computerScore++;
                 return "You lose. Rock beats Scissor.";
             }
             break;
     }
 }
 
-console.log(playRound(playerSelection,computerSelection));
+
+
+
+//Game functon that plays 5 rounds and keeps score. Calls inputVerify() helper function to verify player inputs before proceeding. 
+
+
+function game(){
+
+    let count = 0;
+    for (let i = 0; i < 5; i++){
+        const playerSelection = prompt("Choose Rock, Paper, or Scissor");
+        const computerSelection = getComputerChoice();
+
+        let playerLowerCase = playerSelection.toLowerCase();
+
+        if (inputVerify(playerLowerCase) === false){
+            i--;
+        }else {
+            console.log(playRound(playerLowerCase, computerSelection));
+            count++;
+            console.log("Game:  " + count + "\nPlayer:  " + playerScore + "  " + "Computer:  " + computerScore);
+        }
+    }
+
+    if (playerScore > computerScore) {
+        return "You won. You beat the computer " + playerScore + " to " + computerScore; 
+    }else if (playerScore === computerScore){
+        return "Tie score! You both won " + playerScore + " " + "games!";
+    }else {
+        return "You lost. The computer beat you " + computerScore + " to " + playerScore;
+    }
+}
+
+console.log(game());
