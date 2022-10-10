21bfb77353a35a1914c745fa7aea20c392dba8c0
marefpceo <marefpceo@gmail.com>
2022-10-09 20:33:01 -0700

    Game functionality for rock-paper-scissor complete
    Create a function that randomly selects a choice for the computer
    
    Used a prompt to get the players input.
    
    Created a function that declares the winner based on the input of the
    player and the computer.

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 //rock-paper-scissors game will be played against the computer
-
-const cpuChoice = null;
+//The game will consist of a single round of game play. 
 
 //Function that randomly makes computer selection
 function getComputerChoice() {
@@ -8,24 +7,75 @@ function getComputerChoice() {
     let selection;
 
     if (x === 0){
-        selection = "Rock";
+        selection = "rock";
     }
     else if(x === 1){
-        selection = "Paper";
+        selection = "paper";
     }
     else {
-        selection = "Scissor";
+        selection = "scissor";
     }
     return selection;
 }
 
-console.log(getComputerChoice());
 
+//Prompt the player to make a selection
+const playerSelection = prompt("Choose Rock, Paper, or Scissor");
 
-
-
-//The game will consist of a single round of game play. 
-//The player will make a selection
 //The computer's selection will be randomly generated 
-//A winner will be declared based on the two selections
+const computerSelection = getComputerChoice();
 
+//A winner will be declared based on the two selections
+//Rock beats Scissors
+//Paper beats Rock
+//Scissor beat Paper
+
+function playRound(playerSelection, computerSelection){
+    let player = playerSelection.toLowerCase();
+
+
+    if ((playerSelection !== "rock") && (playerSelection !== "paper") && (playerSelection !== "scissor")) {
+        alert("Please enter a valid selection!");
+        location.reload();
+    }
+
+    switch (player) {
+        case "rock":
+            if (player === computerSelection) {
+                return "The game was tied. You both selected Rock";
+            } 
+
+            if (computerSelection === "scissor") {
+                return "You win! Rock beats Scissor!";
+            } else {
+                return "You lose. Paper beats Rock.";
+            }
+            break;
+    
+        case "paper":
+            if (player === computerSelection) {
+                return "The game was tied. You both selected Paper";
+            } 
+
+            if (computerSelection === "rock") {
+                return "You win! Paper beats Rock!";
+            } else {
+                return "You lose. Scissor beats Paper.";
+            }
+            break;
+
+        case "scissor":
+            if (player === computerSelection) {
+                return "The game was tied. You both selected Scissor";
+            } 
+
+            if (computerSelection === "paper") {
+                return "You win! Scissor beats Paper!";
+            } else {
+                return "You lose. Rock beats Scissor.";
+            }
+            break;
+    }
+}
+
+console.log(playRound(playerSelection,computerSelection));
