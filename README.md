678ce4ea03fee3c877ad1da77fa660b37c407b56
marefpceo <marefpceo@gmail.com>
2022-10-16 19:13:16 -0700

    Completed GUI
    Completed the GUI and tested functionality.
    
    README.md file has been updated with new screenshot.
    
    Custom font was added to /source/fonts.
    
    Updates made to index.html.
    
    Created style.css and completed styling.

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-# rock-paper-scissors
-Rock Paper Scissors game  v.1
+# Rock-Paper-Scissors
+## **Rock Paper Scissors game  v.1**
 
 Version 1 (v.1) is an expansion from the the simple implementation of the classic "rock, paper, scissors" game. The new version has moved from the text and console game play to a GUI interface. 
 
-![Screenshot of GUI] (/images/rps-ui.png)
+Screenshot:
+![Screenshot](/images/rps-ui.png "Rock-Paper-Scissors (v.1) GUI")
+
+### Gameplay
+
+To start the game, the user will select one of the 3 icons. The computer will make a selection and notification will be given.
+
+The game will continue until either the Player or the CPU wins 5 rounds. 
+
+The 'Reset' button will restart the next game. 
--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const btnReset = document.querySelector('#btnReset');
 const results = document.querySelector('#results');
 const player = document.querySelector('#player').querySelector('p');
 const computer = document.querySelector('#computer').querySelector('p');
+const playerIcon = document.createElement("img");
+const computerIcon = document.createElement("img");
+const selection = document.getElementById("selection");
 
 let playerScore = 0;
 let computerScore = 0;
@@ -25,6 +28,7 @@ function getComputerChoice() {
     return selection;
 }
 
+
 //Function determines winner and disable selection buttons
 function gameWinner(playerScore, computerScore) {
     if (playerScore === 5) {
@@ -36,8 +40,23 @@ function gameWinner(playerScore, computerScore) {
     }
 }
 
+
+//Displays the current selection for each player. 
+function displaySelection(playerSelection, computerSelection) {
+    let playerChoice = playerSelection.toLowerCase();
+    let computerChoice = computerSelection.toLowerCase();
+    
+    playerIcon.src = `/images/${playerChoice}-orange.svg`;
+    computerIcon.src = `/images/${computerChoice}-orange.svg`;
+    selection.appendChild(playerIcon);
+    selection.appendChild(computerIcon);
+}
+
+
 //Function to play a single round of the rock, paper, scissor game
 function playRound(playerSelection, computerSelection){
+
+        displaySelection(playerSelection, computerSelection);
 
         if (playerSelection === computerSelection){
             results.textContent = `The game was tied. You both selected ${playerSelection}`;
@@ -65,18 +84,23 @@ function disablePlayButtons() {
     document.getElementById('Scissor').disabled = true;
 }
 
+
+//Adds event listeners to each button and starts the game by calling playRound(
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         playRound(button.id, getComputerChoice());
-        // player.textContent = playerScore;
-        // computer.textContent = computerScore;
     });
 });
 
+
+//Resets everything back to their initial state
 btnReset.addEventListener('click', () => {
-    results.textContent = '';
+    results.textContent = 'Make a selection to begin';
     player.textContent = playerScore = 0;
     computer.textContent = computerScore = 0;
+
+    selection.removeChild(playerIcon);
+    selection.removeChild(computerIcon);
 
     buttons.forEach((button) => {
         button.disabled = false;
